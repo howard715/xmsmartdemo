91558a497ebd243fb4d36f81d5b6fc97cc112c28
howard <howard0715@126.com>
2018-01-30 15:44:42 +0800

    delete

--- mqttxpg.h
+++ mqttxpg.h
@@ -1,16 +1,1 @@
 
-#ifndef __MQTTLIB_H__
-#define __MQTTLIB_H__
-
-#include "xmsocket.h"
-#define MQTT_STATUS_LOGIN           (1<<1)
-#define MQTT_STATUS_LOGINTOPIC     (1<<3)
-#define MQTT_STATUS_RUNNING         (1<<4)
-
-void Connect_cloud();
-void xm_SendPublishPacket(uint8 *uartbuf,int uartbufLen);
-void xm_SendData2APP(xm_soc_t *xm_soc,uint8 *uartbuf,int uartbufLen);
-int _xm_SendData2APP(xm_soc_t* xm_soc,uint8 *uartbuf,int uartbufLen,uint8 cmd);
-void xm_SendData2Dev(uint8 *uartbuf,int uartbufLen,uint8 *devsn);
-void cloud_data_recv(void *param);
-#endif // __LIBEMQTT_H__
--- server.h
+++ server.h
@@ -1,17 +1,1 @@
-#ifndef __SERVER_H_
-#define __SERVER_H_
-#include "xmplatform.h"
-#include "xmqueue.h"
 
-#define SDK_VERSION "v2.1"
-
-extern uint32 cloud_server_ip;
-extern xm_conf *xcf;
-
-int ch2int(char ch);
-void BuildDataHeader(uint8* SendPack,uint8 cmd,int datalen,int handle);
-int parse_content_len(char *pdata);
-void xm_event_call(event_cmd event,void *param);
-
-
-#endif
